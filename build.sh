c8f0aadd2c013b7723f78f2938ab3ab864f1257a
Ravishankar Bhagavandas <bhagavar@amazon.com>
2022-05-18 20:10:18 +0000

    Move mutual auth demo to parent folder

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-csolution convert -s IoT.csolution.yml -c AWS_MQTT_MutualAuth_Demo.Release+AVH_MPS3_Corstone-300
-cbuild aws_mqtt_mutual_auth/AWS_MQTT_MutualAuth_Demo.Release+AVH_MPS3_Corstone-300.cprj 
+case $1 in
+    "-c")
+        cbuild aws_mqtt_mutual_auth/AWS_MQTT_MutualAuth_Demo.Release+AVH_MPS3_Corstone-300.cprj -c
+        ;;
+    *)
+        csolution convert -s IoT.csolution.yml -c AWS_MQTT_MutualAuth_Demo.Release+AVH_MPS3_Corstone-300
+        cbuild aws_mqtt_mutual_auth/AWS_MQTT_MutualAuth_Demo.Release+AVH_MPS3_Corstone-300.cprj
+        ;;
+esac
