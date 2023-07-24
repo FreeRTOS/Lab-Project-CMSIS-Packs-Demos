79b303da6ec56f98a19382041ac19abc6058f7a3
Richard Elberger <elberger@amazon.com>
2023-07-24 14:21:56 -0400

    wrong command for dirname

--- install_cpacks.sh
+++ install_cpacks.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-d=$(basename $0)
+d=$(dirname $0)
 c="cpackget -q pack add -a"
 # Use the single source of truth to get packages
 plist=$(grep pack: $d/IoT.csolution.yml  | sed -e 's/\s//g' -e s'/-pack://g')
