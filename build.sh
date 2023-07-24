df5925038b0e131a996e4cf6d8d0bfb2cf0449f8
Richard Elberger <elberger@amazon.com>
2023-07-24 14:28:26 -0400

    generated FreeRTOSConfig.h doesn't work, forcibly copy working one

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ case $1 in
         cbuild mqtt_pub_sub/AWS_MQTT_PubSub_Demo.Release+AVH_MPS3_Corstone-300.cprj -c
         ;;
     *)
-        csolution convert -s IoT.csolution.yml -c AWS_MQTT_PubSub_Demo.Release+AVH_MPS3_Corstone-300
+	csolution convert -s IoT.csolution.yml -c AWS_MQTT_PubSub_Demo.Release+AVH_MPS3_Corstone-300
+	cp mqtt_pub_sub/RTE/RTOS/FreeRTOSConfig.h RTE/RTOS/FreeRTOSConfig.h
         cbuild mqtt_pub_sub/AWS_MQTT_PubSub_Demo.Release+AVH_MPS3_Corstone-300.cprj
         ;;
 esac
